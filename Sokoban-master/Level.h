07d0e288f858dd12cdca73cc51d536b0ba7fd43b
Mckinlaydnl <43002081+Mckinlaydnl@users.noreply.github.com>
2018-11-28 13:17:30 +0000

    Initial commit

---
+++ Sokoban-master/Level.h
@@ -0,0 +1,32 @@
+#pragma once
+
+// Project Includes
+#include "GridObject.h"
+
+// Library Includes
+#include <SFML/Graphics.hpp>
+
+class Level
+{
+
+public:
+
+	Level();
+
+	void Draw(sf::RenderTarget& _target);
+	void Update(sf::Time _frameTime);
+
+	void LoadLevel(int _levelToLoad);
+	void ReloadLevel();
+	void LoadNextLevel();
+
+	float GetCellSize();
+
+private: 
+
+	const float m_cellSize;
+	int m_currentLevel;
+	std::vector< std::vector< sf::Sprite > > m_background;
+	std::vector< std::vector< std::vector< GridObject* > > > m_contents;
+
+};
